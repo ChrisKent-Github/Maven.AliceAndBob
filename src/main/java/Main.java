1d12e2c19ca0a7672102c6c56903a0994af2d279
Chris <chris@Zipcoders-MBP-4.hsd1.de.comcast.net>
2022-06-27 21:32:29 -0400

    Completed Project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Who goes there? ");
+        String name = in.nextLine();
+        if (name.toLowerCase().matches("alice|bob")){
+            System.out.println("Hello There");
+        } else
+        {
+
+        }
+
 
     }
 }
